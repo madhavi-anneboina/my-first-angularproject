a768b9ad63fd22181203a6382aefb2095844c8f4
madhavi-anneboina <128103581+madhavi-anneboina@users.noreply.github.com>
2023-04-26 14:32:08 -0400

    added observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component,ViewChild, ViewChildren } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 
 @Component({
@@ -11,6 +13,24 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
 constructor(private http:HttpClient){}
 
+// Content about observable 
+myobs = new Observable(
+  (listner) =>{
+    listner.next("hellpppp")
+    listner.next(2)
+    setTimeout(() => listner.next(3),1000);
+    setTimeout(() => listner.next(4),1000);
+
+    setTimeout(() => listner.next(6),1000);
+    setTimeout(() => listner.complete(),1000);
+})
+
+subscribe(){
+  this.myobs.subscribe(
+    data => {console.log(data)},
+    err => {console.log(err)},
+    () => {console.log("completed")}
+  )}
 data :any
 getData(){
   this.http.get("https://fakestoreapi.com/products")
